db804adab3193598f14318f95042d4d8e1f6f96c
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2024-12-04 10:18:37 +0700

    Edit structure

--- Module_3/task_3/shared_memory.h
+++ Module_3/task_3/shared_memory.h
@@ -9,17 +9,28 @@
 #include <time.h>
 #include <unistd.h>
 
+// Файл для генерации ключа
 #define SHM_FILE "keyfile"
+
+// ID для генерации ключа
 #define ID 2
 
+// Порог времени
+#define TIME_LIMIT 60
+
 typedef struct {
-    time_t white_time; // Время белых
-    time_t black_time; // Время чёрных
-    int player; // Чей ход : 0 - чёрные, 1 - белые
+    time_t last_white_move_time; // Время последнего хода белых
+    time_t last_black_move_time; // Время последнего хода черных
+    time_t white_total_time; // Суммарное время белых
+    time_t black_total_time; // Суммарное время черных
+    int current_turn; // 0 - ход белых, 1 - ход черных
+    int time_limit_exceeded; // Флаг для технического поражения
 } ChessClock;
 
+// Подключение к разделяемой памяти
 ChessClock* connect_shared_memory();
 
+// Отключение от разделяемой памяти
 int disconnect_shared_memory(ChessClock* clock);
 
 #endif
